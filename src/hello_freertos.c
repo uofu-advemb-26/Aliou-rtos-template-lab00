82f8f667b8acd02acb6ba04b40548783fb12e205
aliou tippett <u1415075@umail.utah.edu>
2026-09-09 14:26:23 -0600

    I forgot to flash the working code onto the physical Pico board to generate the required test results/logs.

--- src/hello_freertos.c
+++ src/hello_freertos.c
@@ -27,6 +27,7 @@ void blink_task(__unused void *params) {
         cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, on);
         if (count++ % 11) on = !on;
         vTaskDelay(1000); // 1 seconds
+        // Triggering a fresh build check
     }
 }
 
